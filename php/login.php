1f87f79a3b7da74f1288c57ff043af4e4327bf93
Adilson Alves <adilson.day@hotmail.com>
2016-06-03 15:05:45 -0300

    Estrutura HTML no login.php
    Arruma erro de caracteres

--- php/login.php
+++ php/login.php
@@ -1,20 +1,29 @@
-<?php
-    include("conectar.php");
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+    </head>    
     
-    $usuario = $_POST['user'];
-    $senha = $_POST['pwd'];
+    <body>
+    <?php
+        include("conectar.php");
+        
+        $usuario = $_POST['user'];
+        $senha = $_POST['pwd'];
+        
+        //tabela usuario/cliente
+        $busca = mysql_query("SELECT FROM cliente WHERE usuario='$usuario' AND senha='$senha'");
+        
+        if(mysql_affected_rows >= 1){
+            session_start();
+            $_SESSION["usuario"] = $usuario;
+            $_SESSION["senha"] = $senha;
+            header("../index.html"); //nao pode usuar header se já deu um echo na pagina
+        }
+        else {
+            echo ('<p>Usuário não cadastrado, <a href="../conta_mi_registrar.html">clique aqui</a> para cadastrar');
+        }
     
-    //tabela usuario/cliente
-    $busca = mysql_query("SELECT FROM cliente WHERE usuario='$usuario' AND senha='$senha'");
-    
-    if(mysql_affected_rows >= 1){
-        session_start();
-        $_SESSION["usuario"] = $usuario;
-        $_SESSION["senha"] = $senha;
-        header("../index.html"); //nao pode usuar header se já deu um echo na pagina
-    }
-    else {
-        echo ('<p>Usuário não cadastrado, <a href="../conta_mi_registrar.html">clique aqui</a> para cadastrar');
-    }
-
-?>
+    ?>
+    </body>
+</html>
