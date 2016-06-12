1e1e36fc647582bf9e00335d4fa6fe2007997158
pattycuti <wenndy.San@hotmail.com>
2016-06-12 20:13:55 -0300

    Update cadastro.php

--- php/cadastro.php
+++ php/cadastro.php
@@ -21,8 +21,6 @@ if($senha == $senhanovamente){
     
     if($login == "" || $login == null){
         echo"<script language='javascript' type='text/javascript'>alert('O campo login deve ser preenchido');window.location.href='http://projetoxiaomi.azurewebsites.net/conta_mi_registrar.html';</script>";
-    } else if ($senha == "" || $senhanovamente == null  || $senhanovamente == "" || $senha == null){
-        echo"<script language='javascript' type='text/javascript'>alert('O campo de senha deve ser preenchido');</script>";
         }else{
             if($logarray == $login){
  
